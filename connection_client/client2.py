ce3688c576e4b95ad55a14e2fdb58c37ca9b7628
Ubuntu <ubuntu@ip-172-31-38-9.eu-central-1.compute.internal>
2019-03-11 12:30:34 +0000

    connection_client/

---
+++ connection_client/client2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import socket                   # Import socket module
+
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+port = 60387                    # Reserve a port for your service.
+
+print (gethostname())
+
+s.connect((host, port))
+s.send("Hello server!")
+
+with open('received_file', 'wb') as f:
+    print ('file opened')
+    while True:
+        print('receiving data...')
+        data = s.recv(1024)
+        print('data=%s', (data))
+        if not data:
+            break
+        # write data to a file
+        f.write(data)
+
+f.close()
+print('Successfully get the file')
+s.close()
+print('connection closed')
