ee17fde8bdec9a0cd89cb75c97e495aecf1a6229
Ubuntu <ubuntu@ip-172-31-46-89.eu-central-1.compute.internal>
2019-03-19 19:15:14 +0000

    file cleaning

--- connection_server/server.py
+++ connection_server/server.py
@@ -6,29 +6,26 @@ p = 60387  # Reserve a port for your service.
 s = socket.socket()  # Create a socket object
 server = socket.gethostname()  # Get local machine name
 s.bind((server, p))  # Bind to the port
-s.listen(5)  # Now wait for client connection.
+s.listen(2)  # Now wait for client connection.
 
 
 print(server)
 print("Server listening....")
 
 while True:
-    conn, addr = s.accept()     # Establish connection with client.
+    conn, addr = s.accept()  # Establish connection with client.
     print("Got connection from", addr) 
-    data = conn.recv(1024)
-    print("Server received", repr(data))
 
-    filename='received.txt'
+    # open file
+    filename='received_'+str(addr[0])+'.txt'
     f = open(filename, 'w')
+    
+    # receive data and write it to file
+    data = conn.recv(1024)
+    print("Server received", str(data))
     f.write(repr(data))
-#     f = open(filename,'rb')
-#     l = f.read(1024)
-#     while (l):
-#        conn.send(l)
-#        print('Sent ',repr(l))
-#        l = f.read(1024)
     f.close()
 
+    # close server
     print("Done receiving")
-#     conn.send("Thank you for connecting")
-#     conn.close()
+    conn.close()
