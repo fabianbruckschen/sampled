1266f247c84164a21adcce3b29e0adc5bda0c72f
Ubuntu <ubuntu@ip-172-31-46-89.eu-central-1.compute.internal>
2019-03-25 11:56:44 +0000

    execution files + pickle function

---
+++ read_pickle.py
@@ -0,0 +1,8 @@
+# import necessary modules
+import pickle  # efficient file saving & loading
+
+# reading and decoding binary pickles
+def read_pickle(file_name):
+    file = open(file_name, 'rb')
+    data = pickle.load(file)
+    return pickle.loads(data)
